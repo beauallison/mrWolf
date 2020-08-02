ce714c8d11e67ad146186732964c11035392676e
Beau Allison <beauallison@users.noreply.github.com>
2020-08-02 14:32:32 +0100

    Fix filename

--- src/components/Settings/CitySelector.tsx
+++ src/components/Settings/CitySelector.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Select from 'react-select';
 import { FixedSizeList as List } from 'react-window';
-import { Styles } from './CitySelectStyles';
+import { Styles } from './CitySelectorStyles';
 import { IValue, cityToValue, citiesToValues, generateOptions, valueToCity, valuesToCities } from './utils';
 import ICity from '../../ICity';
 import * as LocalStorage from '../../localStorage';
