34fc77e022a41befb111045e1cfe4e0ed98683f9
Beau Allison <beauallison@users.noreply.github.com>
2020-08-14 17:29:54 +0100

    Add keys for storage options

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -9,6 +9,9 @@ const KEY_HOME = 'KEY_HOME';
 const KEY_CITIES = 'KEY_CITIES';
 const KEY_DISPLAY_24HR_TIME = 'KEY_DISPLAY_24HR_TIME';
 const KEY_DISPLAY_SECONDS = 'KEY_DISPLAY_SECONDS';
+const KEY_COLOR_PRIMARY = 'KEY_COLOR_PRIMARY';
+const KEY_COLOR_SECONDARY = 'KEY_COLOR_SECONDARY';
+const KEY_DISPLAY_FONT_SIZE = 'KEY_DISPLAY_FONT_SIZE';
 
 export const saveHome = (home: ICity) => Storage.saveItem(KEY_HOME, home);
 export const getHome = () => Storage.getItem(KEY_HOME) as ICity;
@@ -22,4 +25,22 @@ export const getDisplay24HrTime = () => Storage.getItem(KEY_DISPLAY_24HR_TIME) a
 export const saveDisplaySeconds = (display: boolean) => Storage.saveItem(KEY_DISPLAY_SECONDS, display);
 export const getDisplaySeconds = () => Storage.getItem(KEY_DISPLAY_SECONDS) as boolean;
 
-export const getAll = () => Storage.getAll([KEY_HOME, KEY_CITIES, KEY_DISPLAY_24HR_TIME, KEY_DISPLAY_SECONDS]);
+export const saveDisplayFontSize = (fontSize: string) => Storage.saveItem(KEY_DISPLAY_FONT_SIZE, fontSize);
+export const getDisplayFontSize = () => Storage.getItem(KEY_DISPLAY_FONT_SIZE) as boolean;
+
+export const saveColorPrimary = (color: string) => Storage.saveItem(KEY_COLOR_PRIMARY, color);
+export const getColorPrimary = () => Storage.getItem(KEY_COLOR_PRIMARY);
+
+export const saveColorSecondary = (color: string) => Storage.saveItem(KEY_COLOR_SECONDARY, color);
+export const getColorSecondary = () => Storage.getItem(KEY_COLOR_SECONDARY);
+
+export const getAll = () =>
+  Storage.getAll([
+    KEY_HOME,
+    KEY_CITIES,
+    KEY_DISPLAY_24HR_TIME,
+    KEY_DISPLAY_SECONDS,
+    KEY_DISPLAY_FONT_SIZE,
+    KEY_COLOR_PRIMARY,
+    KEY_COLOR_SECONDARY,
+  ]);
