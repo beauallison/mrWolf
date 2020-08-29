90f58e2a478c973b4e2ffb1a93fa431941138dba
Beau Allison <beauallison@users.noreply.github.com>
2020-08-29 18:27:26 +0100

    Add Checklist to Main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 import styled from '@emotion/styled';
 import Home from './Home';
 import Cities from './Cities';
+import Checklist from '../Checklist';
 import ICity from '../../ICity';
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  display: grid;
+  grid-template-columns: ${(props) => (props.checklist ? '70% 30%' : '100%')};
 
   height: 100vh;
   width: 100vw;
+`;
+
+const TimeViewer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 export interface IProps {
@@ -20,9 +26,27 @@ export interface IProps {
   displaySeconds?: boolean;
 }
 
+const tempChecklist = [
+  {
+    name: 'ABC',
+    complete: true,
+  },
+  {
+    name: 'XYZ',
+    complete: false,
+  },
+  {
+    name: '123',
+    complete: false,
+  },
+];
+
 export default ({ home, cities, ...props }: IProps) => (
-  <Container>
-    <Home {...home} {...props} />
-    {cities && <Cities cities={cities} {...props} />}
+  <Container checklist={tempChecklist}>
+    <TimeViewer>
+      <Home {...home} {...props} />
+      {cities && <Cities cities={cities} {...props} />}
+    </TimeViewer>
+    {tempChecklist && <Checklist checklist={tempChecklist} />}
   </Container>
 );
