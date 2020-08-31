a0a30d8c283f15d5dab478ea85c1f893b06021be
Beau Allison <beauallison@users.noreply.github.com>
2020-08-31 15:08:27 +0100

    Add delete button

--- src/components/Checklist/Item.tsx
+++ src/components/Checklist/Item.tsx
@@ -3,11 +3,29 @@ import styled from '@emotion/styled';
 import IChecklist from '../../IChecklist';
 import Checkbox from './Checkbox';
 
+const DeleteButton = styled.button`
+  display: none;
+  background-color: ${(props) => props.theme.colors.background};
+  border: solid 1px ${(props) => props.theme.colors.onBackground};
+  border-radius: 5px;
+  width: 25px;
+  height: 25px;
+
+  color: ${(props) => props.theme.colors.onBackground};
+  font-size: 12px;
+`;
+
 const ChecklistItem = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-columns: 10% 80% 10%;
+
   align-items: center;
 
-  max-width: 200px;
+  max-width: 250px;
+
+  &:hover ${DeleteButton} {
+    display: block;
+  }
 `;
 
 const Title = styled.p`
@@ -21,11 +39,13 @@ const Title = styled.p`
 
 export interface IChecklistItem extends IChecklist {
   toggleComplete: void;
+  deleteItem: void;
 }
 
 export default (props: IChecklistItem) => (
   <ChecklistItem>
     <Checkbox checked={props.complete} onChange={props.toggleComplete} />
     <Title complete={props.complete}>{props.name}</Title>
+    <DeleteButton onClick={props.deleteItem}>X</DeleteButton>
   </ChecklistItem>
 );
