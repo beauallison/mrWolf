0bad53a37c4cd33c92846448941cf39c0ed7458a
Beau Allison <beauallison@users.noreply.github.com>
2020-09-13 10:20:55 +0100

    Add version to storage

--- src/pages/props.ts
+++ src/pages/props.ts
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import * as Storage from '../storage';
 import { colorSchemes } from '../components/theme';
 import IChecklist from '../IChecklist';
+import Package from '../../package.json';
+
+const CURRENT_VERSION = Package.version;
 
 export default () => {
   const [loaded, setLoaded] = useState(false);
@@ -15,6 +18,7 @@ export default () => {
   const [colorSecondary, setColorSecondary] = useState('#03DAC6');
   const [displayChecklist, setDisplayChecklist] = useState(false);
   const [checklist, setChecklist] = useState([]);
+  const [displayNewVersionModal, setDisplayNewVersionModal] = useState(false);
 
   const toggleDisplay24HourTime = async () => {
     const value = !display24HourTime;
@@ -75,6 +79,13 @@ export default () => {
     await Storage.saveChecklist(checklist);
   };
 
+  const checkVersion = async (version: string) => {
+    if(version !== CURRENT_VERSION) {
+      await setDisplayNewVersionModal(true);
+      await Storage.saveVersion(CURRENT_VERSION);
+    }
+  }
+
   async function load() {
     if (loaded) return;
     const result = await Storage.getAll();
@@ -89,6 +100,7 @@ export default () => {
       KEY_COLOR_SECONDARY,
       KEY_DISPLAY_CHECKLIST,
       KEY_CHECKLIST,
+      KEY_VERSION,
     } = result;
 
     KEY_HOME && (await setHome(KEY_HOME));
@@ -101,6 +113,7 @@ export default () => {
     KEY_COLOR_SECONDARY && (await setColorSecondary(KEY_COLOR_SECONDARY));
     KEY_DISPLAY_CHECKLIST && (await setDisplayChecklist(KEY_DISPLAY_CHECKLIST));
     KEY_CHECKLIST && (await setChecklist(KEY_CHECKLIST));
+    await checkVersion(KEY_VERSION);
     await setLoaded(true);
   }
 
