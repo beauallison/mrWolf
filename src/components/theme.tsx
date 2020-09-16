e1217277d2812c0a942f6bbddfd767a06c4293f2
Beau Allison <beauallison@users.noreply.github.com>
2020-09-16 11:29:08 +0100

    Change default theme to Dusk (#25)

--- src/components/theme.tsx
+++ src/components/theme.tsx
@@ -1,7 +1,7 @@
 export const theme = {
   colors: {
-    primary: '#BB86FC',
-    secondary: '#03DAC6',
+    primary: '#C65B5E',
+    secondary: '#D7849F',
     background: '#2A2A2E',
     surface: '#121212',
     onPrimary: '#000000',
@@ -13,13 +13,13 @@ export const theme = {
 
 export const colorSchemes = [
   {
-    name: 'Original',
-    primary: '#BB86FC',
-    secondary: '#03DAC6',
-  },
-  {
     name: 'Dusk',
     primary: '#C65B5E',
     secondary: '#D7849F',
+  },
+  {
+    name: 'Original',
+    primary: '#BB86FC',
+    secondary: '#03DAC6',
   },
 ];
