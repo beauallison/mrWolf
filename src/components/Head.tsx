e8bc8a5eb9f9bccdb5699b870175df544a844397
Beau Allison <beauallison@users.noreply.github.com>
2022-07-10 08:17:39 +0100

    Remove Inner Set HTML

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -2,15 +2,5 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 
 export default ({ title, description, schema }) => (
-  <>
-    <Helmet htmlAttributes={{ lang: 'en' }} title={title} description={description} />
-    {schema && (
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify(schema, null, 4),
-        }}
-      />
-    )}
-  </>
+  <Helmet htmlAttributes={{ lang: 'en' }} title={title} description={description} schema={schema} />
 );
